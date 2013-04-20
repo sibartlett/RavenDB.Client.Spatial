7997dc890008baf0ab0cc6cc4bde4480fc931b9b
Simon Bartlett <simon.bartlett@me.com>
2013-04-20 13:32:23 -0400

    Consolidated AssemblyInfo files

---
+++ Raven.Client.Spatial/CommonAssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Raven.Client.Spatial")]
+[assembly: AssemblyCopyright("Copyright © Simon Bartlett 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
