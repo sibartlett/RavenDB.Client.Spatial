55428341cb67864d0099b505de53309133c61f38
Simon Bartlett <simon.bartlett@me.com>
2013-04-24 23:14:54 -0400

    Added SpatialCriteriaFactory extension methods

---
+++ Raven.Client.Spatial/DotSpatial/SpatialCriteriaFactoryExtensions.cs
@@ -0,0 +1,22 @@
+﻿using DotSpatial.Topology;
+using Raven.Client.Spatial;
+
+namespace Raven.Client
+{
+	public static class SpatialCriteriaFactoryExtensions
+	{
+		public static SpatialCriteria WithinRadiusOf(this SpatialCriteriaFactory @this,
+													double radius,
+													IPoint point)
+		{
+			return @this.WithinRadiusOf(radius, point.Coordinate);
+		}
+
+		public static SpatialCriteria WithinRadiusOf(this SpatialCriteriaFactory @this,
+													double radius,
+													Coordinate coordinate)
+		{
+			return @this.WithinRadiusOf(radius, coordinate.X, coordinate.Y);
+		}
+	}
+}
---
+++ Raven.Client.Spatial/Geo/SpatialCriteriaFactoryExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Geo.Abstractions.Interfaces;
+using Raven.Client.Spatial;
+
+namespace Raven.Client
+{
+	public static class SpatialCriteriaFactoryExtensions
+	{
+		public static SpatialCriteria WithinRadiusOf(this SpatialCriteriaFactory @this,
+													double radius,
+													IPosition position)
+		{
+			var coordinate = position.GetCoordinate();
+			return @this.WithinRadiusOf(radius, coordinate.Longitude, coordinate.Latitude);
+		}
+	}
+}
